66f88076872b0c7cbf3f1befb91214ae9fc6472e
Raymundo Medina <A00838157@tec.mx>
2025-05-16 11:15:59 -0600

    Modificación index

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,56 @@ import Header from './component/Header';
 import Boton from './component/Boton';
 import List from './component/List';
 import Add from './component/Add';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import ResponsiveAppBar from './component/AppBar';
 import CredentialsSignInPae from './component/Login';
 import Login from './component/Login';
 import Greeting from './component/Greeting';
 import Logout from './component/Logout';
 
-function App() {
-  console.log('App component is running');
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+// Obtener la URL base de la API desde las variables de entorno
+const API_URI = process.env.REACT_APP_API_URI;
 
+
+function AppContent() {
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [items, setItems] = useState([]);
 
-  useEffect(()=> {
-    console.log("Autentificación ha cambiado, estado actual: ", isAuthenticated);
-    if(isAuthenticated){
-      console.log("Usuario autentificado");
+  useEffect(() => {
+    // Verificar si hay un token almacenado al cargar la página
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      setIsAuthenticated(true);
     }
-  },[isAuthenticated]);
+  }, []);
+
+  useEffect(() => {
+    console.log("Autentificación ha cambiado, estado actual: ", isAuthenticated);
+    if (isAuthenticated) {
+      console.log("Usuario autentificado");
+      // Si el usuario está en la página principal o de login, redirigir a /add
+      if (window.location.pathname === "/" || window.location.pathname === "/login") {
+        navigate("/add");
+      }
+    } else {
+      // Si el usuario no está autenticado y no está en la página principal, redirigir a /
+      if (window.location.pathname !== "/") {
+        navigate("/");
+      }
+    }
+  }, [isAuthenticated, navigate]);
 
   const getItems = async () => {
     try {
+      const token = localStorage.getItem("authToken");
       const startTime = Date.now();
-      const result = await fetch("http://localhost:50000/items3/", { method: "GET" });
+      const result = await fetch(`${API_URI}/items3/`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
       const data = await result.json();
       const endTime = Date.now();
       console.log("Tiempo de respuesta del GET:", endTime - startTime, "ms");
@@ -39,91 +65,111 @@ function App() {
     }
   };
 
-  let [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-
+  // Agregar las funciones faltantes
   const add = async (item) => {
-    const result = await fetch("http://localhost:50000/items3/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(item)
-    });
-    const newItem = await result.json();
-    console.log(newItem);
-    if (result.ok) {
-      setItems([...items, newItem]);
-    } else {
-      console.error('Error adding item:', newItem);
-    }
-  };
-  
-  
-
-  const del = async (id) => {
     try {
-      const response = await fetch(`http://localhost:5000/items/${id}`, { method: "DELETE" });
+      const token = localStorage.getItem("authToken");
+      const response = await fetch(`${API_URI}/items3/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(item)
+      });
+      
       if (response.ok) {
-        setItems(items.filter((item) => item.item_id !== id)); 
+        // Actualizar la lista de items después de agregar uno nuevo
+        getItems();
+        alert("Item agregado exitosamente");
+      } else {
+        alert("Error al agregar el item");
       }
     } catch (error) {
-      console.error('Error deleting item:', error);
+      console.error("Error al agregar item:", error);
+      alert("Ocurrió un error al agregar el item");
     }
   };
-  
+
+  const del = async (itemId) => {
+    try {
+      const token = localStorage.getItem("authToken");
+      const response = await fetch(`${API_URI}/items3/${itemId}`, {
+        method: "DELETE",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+      
+      if (response.ok) {
+        // Actualizar la lista de items después de eliminar uno
+        getItems();
+        alert("Item eliminado exitosamente");
+      } else {
+        alert("Error al eliminar el item");
+      }
+    } catch (error) {
+      console.error("Error al eliminar item:", error);
+      alert("Ocurrió un error al eliminar el item");
+    }
+  };
 
   const login = async (user) => {
     try {
-      const result = await fetch("http://localhost:50000/login/", {
+      const result = await fetch(`${API_URI}/login/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(user),
       });
       const data = await result.json();
-      console.log(data);
-  
-      if (data.isLogin) {
-        setIsAuthenticated(true);
-        console.log("Login exitoso");
-  
+      console.log("Respuesta completa del servidor:", data);
+
+      // Comprueba si data es un objeto y tiene una propiedad token
+      if (data && data.token) {
         // Guardar el token en localStorage
         localStorage.setItem("authToken", data.token);
-  
-        // Redirigir a la página /add
-        Navigate("/add");
+        
+        // Actualizar el estado de autenticación
+        setIsAuthenticated(true);
+        console.log("Login exitoso");
+        return true;
       } else {
+        console.log("Falló el login. Respuesta del servidor:", data);
         alert("El login falló");
+        return false;
       }
     } catch (error) {
       console.error("Error durante el login:", error);
       alert("Ocurrió un error durante el login");
+      return false;
     }
   };
 
   const logout = () => {
+    localStorage.removeItem("authToken");
     setIsAuthenticated(false);
-    localStorage.removeItem("authToken"); // Eliminar el token
   };
 
   return (
+    <>
+      {isAuthenticated && <ResponsiveAppBar setLogout={logout} />}
+      <Routes>
+        <Route path="/" element={!isAuthenticated ? <Login login={login} /> : <Navigate to="/add" />} />
+        <Route path="/add" element={isAuthenticated ? <Add add={add} /> : <Navigate to="/" />} />
+        <Route path="/items" element={isAuthenticated ? <List items={items} ondelete={del} /> : <Navigate to="/" />} />
+        <Route path="/login" element={!isAuthenticated ? <Login login={login} /> : <Navigate to="/add" />} />
+        <Route path="/greeting" element={isAuthenticated ? <Greeting /> : <Navigate to="/" />} />
+      </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
     <div className="App">
       <BrowserRouter>
-        {isAuthenticated && <ResponsiveAppBar setLogout={logout}/>}
-          <Routes>  
-            <Route path= "/" element={ <Login login={login}/>}/>
-            <Route path="/add" element={<Add add={add} />} />
-            <Route path="/items" element={<List items={items} ondelete={del} />}/>
-            <Route path="/login" element={!isAuthenticated ? <Login login={login} /> : <Greeting />} />
-
-
-        </Routes>
-      {/* {count}
-      <Boton name={'suma'} click={sum}/>
-      <Boton name={'resta'} click={resta}/>
-      <Boton name={'mensaje'} click= {()=>alert("hola")} />
-      <Add add={add} />
-      <List items={items}/> */}
-        <Footer/>
+        <AppContent />
+        <Footer />
       </BrowserRouter>
     </div>
   );
